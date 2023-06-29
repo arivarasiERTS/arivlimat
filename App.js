ac66399c38372fa31bc3a6b5f710c5e92fd03463
arivarasiERTS <arivarasiERTS@gmail.com>
2023-06-29 14:14:52 +0530

    MultiLanguage Localisation

--- App.js
+++ App.js
@@ -1,79 +1,74 @@
-import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
-import { BarCodeScanner } from 'expo-barcode-scanner';
+import React, {useState} from 'react';
+import {View,Text,StyleSheet,TouchableOpacity} from 'react-native';
+import * as Localization from 'expo-localization';
+import { I18n } from 'i18n-js';
+//import { TouchableOpacity } from 'react-native-gesture-handler';
 
-export default function App() {
-  const [hasPermission, setHasPermission] = useState(null);
-  const [scanned, setScanned] = useState(false);
-  const [text, setText] = useState('Not yet scanned')
-
-  const askForCameraPermission = () => {
-    (async () => {
-      const { status } = await BarCodeScanner.requestPermissionsAsync();
-      setHasPermission(status === 'granted');
-    })()
+const translations = {
+  en: {
+    welcome: "hello",
+  },
+  fr:{
+    welcome: "Bonjour",
   }
+};
+const i18n = new I18n(translations);
+i18n.locale = Localization.locale;
+i18n.enableFallback= true;
 
-  // Request Camera Permission
-  useEffect(() => {
-    askForCameraPermission();
-  }, []);
-
-  // What happens when we scan the bar code
-  const handleBarCodeScanned = ({ type, data }) => {
-    setScanned(true);
-    setText(data)
-    console.log('Type: ' + type + '\nData: ' + data)
-  };
-
-  // Check permissions and return the screens
-  if (hasPermission === null) {
-    return (
-      <View style={styles.container}>
-        <Text>Requesting for camera permission</Text>
-      </View>)
-  }
-  if (hasPermission === false) {
-    return (
-      <View style={styles.container}>
-        <Text style={{ margin: 10 }}>No access to camera</Text>
-        <Button title={'Allow Camera'} onPress={() => askForCameraPermission()} />
-      </View>)
-  }
-
-  // Return the View
-  return (
-    <View style={styles.container}>
-      <View style={styles.barcodebox}>
-        <BarCodeScanner
-          onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
-          style={{ height: 400, width: 400 }} />
-      </View>
-      <Text style={styles.maintext}>{text}</Text>
-
-      {scanned && <Button title={'Scan again?'} onPress={() => setScanned(false)} color='tomato' />}
+export default function App(){
+  const [locale,setLocale] = useState(i18n.locale);
+  const changeLocale = (locale) => {
+    i18n.locale= locale;
+    setLocale(locale)};
+  return(
+    <View style ={styles.container}>
+      <Text style ={styles.header}>Welcome</Text>
+      
+      <Text style ={styles.text}>Choose Language</Text>
+      <TouchableOpacity onPress= {()=> changeLocale('en')}>
+      <Text style ={styles.langText}>English</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress= {()=> changeLocale('fr')}>
+      <Text style ={styles.langText}>French</Text>
+      </TouchableOpacity>
+      <Text style ={styles.welcomeText}>{i18n.t('welcome')}</Text>
     </View>
-  );
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  maintext: {
-    fontSize: 16,
-    margin: 20,
-  },
-  barcodebox: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: 300,
-    width: 300,
-    overflow: 'hidden',
-    borderRadius: 30,
-    backgroundColor: 'tomato'
-  }
-});
+  const styles= StyleSheet.create(
+    {
+      container: {
+        flex: 1,
+        backgroundColor:'#fff'
+      },
+      header:{
+        fontSize:32,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        marginBottom: 50,
+        marginTop : 80
+      },
+      text:{
+        fontSize:20,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        margin: 5,
+        color: 'red'
+      },
+      langText:{
+        fontSize:20,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        margin: 5,
+        backgroundColor: '#DDDDDD',
+        color: 'blue'
+      },
+      welcomeText:{
+        fontSize:50,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        margin: 5,
+        color: 'red'
+      }
+    });
