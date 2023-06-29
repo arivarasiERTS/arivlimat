6681ca29fdafcf15c9af907b13ae30866a92b128
arivarasiERTS <arivarasiERTS@gmail.com>
2023-06-29 14:27:59 +0530

    Loading Image

--- App.js
+++ App.js
@@ -1,74 +1,33 @@
-import React, {useState} from 'react';
-import {View,Text,StyleSheet,TouchableOpacity} from 'react-native';
-import * as Localization from 'expo-localization';
-import { I18n } from 'i18n-js';
-//import { TouchableOpacity } from 'react-native-gesture-handler';
+import { Image } from 'expo-image';
+import { StyleSheet, View } from 'react-native';
 
-const translations = {
-  en: {
-    welcome: "hello",
-  },
-  fr:{
-    welcome: "Bonjour",
-  }
-};
-const i18n = new I18n(translations);
-i18n.locale = Localization.locale;
-i18n.enableFallback= true;
+//const blurhash =
+  '|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[';
 
-export default function App(){
-  const [locale,setLocale] = useState(i18n.locale);
-  const changeLocale = (locale) => {
-    i18n.locale= locale;
-    setLocale(locale)};
-  return(
-    <View style ={styles.container}>
-      <Text style ={styles.header}>Welcome</Text>
-      
-      <Text style ={styles.text}>Choose Language</Text>
-      <TouchableOpacity onPress= {()=> changeLocale('en')}>
-      <Text style ={styles.langText}>English</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress= {()=> changeLocale('fr')}>
-      <Text style ={styles.langText}>French</Text>
-      </TouchableOpacity>
-      <Text style ={styles.welcomeText}>{i18n.t('welcome')}</Text>
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Image
+        style={styles.image}
+        source="https://picsum.photos/seed/696/3000/2000"
+       // placeholder={blurhash}
+        contentFit="cover"
+        transition={100}
+      />
     </View>
-  )
+  );
 }
-  const styles= StyleSheet.create(
-    {
-      container: {
-        flex: 1,
-        backgroundColor:'#fff'
-      },
-      header:{
-        fontSize:32,
-        fontWeight: 'bold',
-        textAlign: 'center',
-        marginBottom: 50,
-        marginTop : 80
-      },
-      text:{
-        fontSize:20,
-        fontWeight: 'bold',
-        textAlign: 'center',
-        margin: 5,
-        color: 'red'
-      },
-      langText:{
-        fontSize:20,
-        fontWeight: 'bold',
-        textAlign: 'center',
-        margin: 5,
-        backgroundColor: '#DDDDDD',
-        color: 'blue'
-      },
-      welcomeText:{
-        fontSize:50,
-        fontWeight: 'bold',
-        textAlign: 'center',
-        margin: 5,
-        color: 'red'
-      }
-    });
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  image: {
+    flex: 1,
+    width: '100%',
+    backgroundColor: '#0553',
+  },
+});
