71efe3c9c70576a6c72d09556e9f053f3cd028b7
arivarasiERTS <arivarasiERTS@gmail.com>
2023-07-07 01:49:19 +0530

    Animated image loading

--- App.js
+++ App.js
@@ -1,46 +1,85 @@
-import * as React from 'react';
-import { View, StyleSheet, Button, Text, Pressable } from 'react-native';
-import * as Speech from 'expo-speech';
-import {FF} from './util/config.js';
+import React, { useEffect, useState } from 'react';
+import { View, StyleSheet, Animated, Text } from 'react-native';
 
-export default function App() {
-  const thingToSay = "Reading content 1 text";
-  const speak = () => {
-    Speech.speak(thingToSay);
-  };
-  const thingToSay1 = "Reading content 2 text";
-  const thingToSay2 = "Reading content 3 text";
-  const speak1 = () => {
-    Speech.speak(thingToSay1);
-  };
-  const speak2 = () => {
-    Speech.speak(thingToSay2);
-  };
-return (
-    <View style={styles.container}>
-      <Text style={styles.Texts}>{thingToSay}</Text>
-      <Button title="Start listening" onPress={speak} />
-      {FF.f1 && <Pressable onPress={speak1}
-      >
-      <Text style={styles.Texts}>{thingToSay1}</Text>
-      </Pressable>}
-      {FF.f2 && <Pressable onPress={speak2}
-      >
-      <Text style={styles.Texts}>{thingToSay2}</Text>
-      </Pressable>}
-    </View>
-  );
+const App = () => {
+    const [isVisible, setisVisible] = useState(true);
+
+    const width = new Animated.Value(0);
+    const height = new Animated.Value(0);
+    const IMAGE = 'https://tsc-website-production.s3.amazonaws.com/uploads/2018/05/React-Native.png';
+    //const IMAGE1 = 'https://www.nicesnippets.com/image/nice-logo.png';
+       useEffect(() => {
+        Animated.timing(
+            width,
+            {
+                toValue: 360,
+                duration: 1200,
+                useNativeDriver: false,
+            },
+        ).start();
+
+        Animated.timing(
+            height,
+            {
+                toValue: 100,
+                duration: 1200,
+                useNativeDriver: false,
+            },
+        ).start();
+    }, []);
+
+    const Hide_Splash_Screen = () => {
+        setisVisible(false);
+    }
+
+    useEffect(() => {
+        let myTimeout = setTimeout(() => {
+            Hide_Splash_Screen();
+        }, 3000);
+        return () => clearTimeout(myTimeout);
+    }, []);
+
+    const Splash_Screen = () => {
+        return (
+            <View style={styles.container}>
+                <Animated.Image
+                    source={{ uri: IMAGE }}
+                    style={{
+                        width: width,
+                        height: height,
+                        position: 'absolute',
+                    }}
+                    resizeMode='cover'
+                />
+            </View>
+        );
+    }
+
+    return (
+        <>
+            {
+                (isVisible === true) ? Splash_Screen() : (
+                     <View style={styles.container}>
+                    <Text style={styles.title}>Welcome</Text>
+                    </View>
+                   
+                )
+            }
+        </>
+    );
 }
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    backgroundColor: '#ecf0f1',
-    padding: 8,
-  },
-  Texts: {
-    marginTop: 10,
-    color: 'black',
-    padding:10,
-  }
-})
+    container: {
+        flex: 1,
+        backgroundColor: '#FFF',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    title: {
+        fontSize:23,
+        fontWeight:'800',
+    },
+});
+
+export default App;
