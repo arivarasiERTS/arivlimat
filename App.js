6df55331265c14bedeee1fa7206c866428ed02a6
arivarasiERTS <arivarasiERTS@gmail.com>
2023-07-07 18:53:22 +0530

    Notifications

--- App.js
+++ App.js
@@ -1,143 +1,101 @@
-import React, { useState } from 'react';
-import { SafeAreaView, StyleSheet, View, Text, Button } from 'react-native';
-import { BottomSheet } from 'react-native-btr';
-import { SocialIcon } from 'react-native-elements';
+import Constants from 'expo-constants';
+import * as Notifications from 'expo-notifications';
+import React, { useState, useEffect, useRef } from 'react';
+import { Text, View, Button, Platform, StyleSheet, StatusBar } from 'react-native';
 
-const App = () => {
-    const [visible, setVisible] = useState(false);
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: false,
+  }),
+});
 
-    const toggleBottomNavigationView = () => {
-        setVisible(!visible);
+
+
+export default function App() {
+
+  const [expoPushToken, setExpoPushToken] = useState('');
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();  
+
+  useEffect(() => {
+    registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
+    notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+      setNotification(notification);
+    });
+    responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+      console.log(response);
+    });
+  
+    return () => {
+      Notifications.removeNotificationSubscription(notificationListener.current);
+      Notifications.removeNotificationSubscription(responseListener.current);
     };
+  }, []);
+  
+  async function registerForPushNotificationsAsync() {
+    let token;
+    if (Constants.isDevice) {
+      const { status: existingStatus } = await Notifications.getPermissionsAsync();
+      let finalStatus = existingStatus;
+      if (existingStatus !== 'granted') {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+      }
+      if (finalStatus !== 'granted') {
+        alert('Failed to get push token for push notification!');
+        return;
+      }
+      token = (await Notifications.getExpoPushTokenAsync()).data;
+      console.log(token);
+    } else {
+      alert('Must use physical device for Push Notifications');
+    }
+  
+    if (Platform.OS === 'android') {
+      Notifications.setNotificationChannelAsync('default', {
+        name: 'default',
+        importance: Notifications.AndroidImportance.MAX,
+        vibrationPattern: [0, 250, 250, 250],
+        lightColor: '#FF231F7C',
+      });
+    }
+  
+    return token;
+  }
 
-    return (
-        <SafeAreaView style={styles.container}>
-            <View style={styles.container}>
-                <Button
-                    onPress={toggleBottomNavigationView}
-                    title="Show Bottom Sheet"
-                />
-                <BottomSheet
-                    visible={visible}
-                    onBackButtonPress={toggleBottomNavigationView}
-                    onBackdropPress={toggleBottomNavigationView}
-                >
-                    <View style={styles.bottomNavigationView}>
-                        <View style={styles.bottomNavigationIneerView}>
-                            <Text style={styles.bottomSheetTitle}>
-                                Share Using
-                            </Text>
-                            <View style={styles.iconView}>
-                                <SocialIcon
-                                    type="twitter"
-                                    onPress={() => {
-                                        toggleBottomNavigationView();
-                                        alert('twitter');
-                                    }}
-                                />
-                                <SocialIcon
-                                    type="gitlab"
-                                    onPress={() => {
-                                        toggleBottomNavigationView();
-                                        alert('gitlab');
-                                    }}
-                                />
-                                <SocialIcon
-                                    type="medium"
-                                    onPress={() => {
-                                        toggleBottomNavigationView();
-                                        alert('medium');
-                                    }}
-                                />
-                                <SocialIcon
-                                    type="facebook"
-                                    onPress={() => {
-                                        toggleBottomNavigationView();
-                                        alert('facebook');
-                                    }}
-                                />
-                                <SocialIcon
-                                    type="instagram"
-                                    onPress={() => {
-                                        toggleBottomNavigationView();
-                                        alert('instagram');
-                                    }}
-                                />
-                            </View>
-                            <View style={styles.iconView}>
-                                <SocialIcon
-                                    type="facebook"
-                                    onPress={() => {
-                                        toggleBottomNavigationView();
-                                        alert('facebook');
-                                    }}
-                                />
-                                <SocialIcon
-                                    type="instagram"
-                                    onPress={() => {
-                                        toggleBottomNavigationView();
-                                        alert('instagram');
-                                    }}
-                                />
-                                <SocialIcon
-                                    type="gitlab"
-                                    onPress={() => {
-                                        toggleBottomNavigationView();
-                                        alert('gitlab');
-                                    }}
-                                />
-                                <SocialIcon
-                                    type="twitter"
-                                    onPress={() => {
-                                        toggleBottomNavigationView();
-                                        alert('twitter');
-                                    }}
-                                />
-                                <SocialIcon
-                                    type="medium"
-                                    onPress={() => {
-                                        toggleBottomNavigationView();
-                                        alert('medium');
-                                    }}
-                                />
-                            </View>
-                        </View>
-                    </View>
-                </BottomSheet>
-            </View>
-        </SafeAreaView>
-    );
+  const sendMesaage = (token) => {
+    fetch('https://exp.host/--/api/v2/push/send', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Accept-encoding': 'gzip, deflate',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        to: token,
+        title: 'Notification',
+        body: 'Subscribe Now',
+        data: { data: 'goes here' },
+        _displayInForeground: true,
+      }),
+    });
+  }
+  return (
+    <View style={styles.container}>
+      <Button title="Notify" onPress={() => sendMesaage(expoPushToken)} />
+      <StatusBar style="auto" />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        margin: 2,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#E0F7FA',
-    },
-    bottomNavigationView: {
-        backgroundColor: '#fff',
-        width: '100%',
-        height: 250,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    bottomSheetTitle: {
-        textAlign: 'center',
-        padding: 20,
-        fontSize: 20
-    },
-    bottomNavigationIneerView: {
-        flex: 1,
-        flexDirection: 'column',
-        justifyContent: 'space-between',
-    },
-    iconView: {
-        flex: 1, 
-        flexDirection: 'row',
-    }
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
-
-export default App;
