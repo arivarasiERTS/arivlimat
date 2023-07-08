065d06b213765724039960e0e8d405aef4ff3890
arivarasiERTS <arivarasiERTS@gmail.com>
2023-07-08 05:58:00 +0530

    Share

--- App.js
+++ App.js
@@ -1,100 +1,35 @@
-import * as React from 'react'; 
-import { SafeAreaProvider,SafeAreaView} from 'react-native-safe-area-context';
-import {
-    Text, View,
-    StyleSheet, FlatList, ActivityIndicator, Platform,
-    } from 'react-native';
-import { SearchBar } from 'react-native-elements';
-import {Card} from 'react-native-paper';
- export default class App extends React.Component { constructor(props) {
-    super(props);
-    this.state = { search: '', isLoading: true }; this.arrayholder = [];
+import React, { Component } from 'react';
+import { Share, View, Button,Text } from 'react-native';
+
+class ShareExample extends Component {
+  onShare = async () => {
+    try {
+      const result = await Share.share({
+        message:
+          'https://www.nicesnippets.com/',
+      });
+
+      if (result.action === Share.sharedAction) {
+        if (result.activityType) {
+          // shared with activity type of result.activityType
+        } else {
+          // shared
+        }
+      } else if (result.action === Share.dismissedAction) {
+        // dismissed
+      }
+    } catch (error) {
+      alert(error.message);
     }
-    componentDidMount() {
-    return fetch('https://jsonplaceholder.typicode.com/posts')
-    .then(response => response.json())
-    .then(responseJson => { this.setState(
-    {
-    isLoading: false, dataSource: responseJson,
-    },
-    function() {
-    this.arrayholder = responseJson;
-    }
-    );
-    })
-    .catch(error => { console.error(error);
-    });
-    }
-    search = text => { console.log(text);
-    };
-    clear = () => { this.search.clear();
-    };
-    SearchFilterFunction(text) {
-    const newData = this.arrayholder.filter(function(item) { const itemData = item.title ? item.title.toUpperCase() :
-    ''.toUpperCase();
-    const textData = text.toUpperCase(); return itemData.indexOf(textData) > -1;
-    });
-    this.setState({ dataSource: newData, search: text,
-    });
-    }
-    ListViewItemSeparator = () => { return (
-    <View
-    style={{ height: 0.4,
-    width: '89%',
-    backgroundColor: '#141313',
-    }}
-    />
-    );
-    };
-    render() {
-    if (this.state.isLoading) { return (
-    <View style={{ flex: 1, paddingTop: 21 }}>
-    <ActivityIndicator />
-    </View>
-    );
-    }
+  };
+
+  render() {
     return (
-    <SafeAreaProvider>
-    <SafeAreaView>
-    <Card style={styles.cardbox}>
-    <SearchBar round
-    searchIcon={{ size: 25 }}
-    onChangeText={text => this.SearchFilterFunction(text)} onClear={text => this.SearchFilterFunction('')} placeholder="Type Here to Search..." value={this.state.search}
-    />
-    <FlatList data={this.state.dataSource}
-    ItemSeparatorComponent={this.ListViewItemSeparator} renderItem={({ item }) => (
-    <Text style={styles.textStyle}>{item.title}</Text>
-    )}
-    enableEmptySections={true} style={{ marginTop: 11 }}
-    keyExtractor={(item, index) => index.toString()}
-    />
-    </Card>
-    </SafeAreaView>
-    </SafeAreaProvider>
+      <View style={{ margin: 20 }}>
+        <Text style={{ marginVertical:40,fontSize: 18 }}>React Native Share link Example</Text>
+        <Button onPress={this.onShare} title="Share" />
+      </View>
     );
-    }
-    }
-    const styles = StyleSheet.create({     
-        container: {
-        flex: 1,
-        textAlign: 'center',
-        justifyContent: 'center',
-        marginHorizontal: 10,
-        marginVertical: 15,
-    },
-    cardbox:{
-        marginVertical:8,
-        marginHorizontal:5,
-        padding: 10,
-    },
-    title:{
-        backgroundColor: '#e2e2e2',
-        fontWeight: 'bold', 
-        textAlign:  'center',
-        borderRadius: 10,
-        paddingVertical: 5 ,
-    },
-    textStyle: { padding: 11,
-    },
-    });
-    
+  }
+}
+export default ShareExample;
