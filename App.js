b0d7976558418c423c5c4f9189901ceebd892a27
arivarasiERTS <arivarasiERTS@gmail.com>
2023-08-07 14:33:40 +0530

    Text to Speech

--- App.js
+++ App.js
@@ -1,119 +1,239 @@
-import React,{useState} from 'react';
-import './assets/i18n/i18n';
-import {View, Text,Pressable} from 'react-native';
-import {useTranslation} from 'react-i18next';
+// import React in our code
+import React, {useState, useEffect} from 'react';
 
+// import all the components we are going to use
 import {
+  SafeAreaView,
   StyleSheet,
+  Text,
+  View,
+  FlatList,
+  TextInput,
+  Keyboard,
   TouchableOpacity,
-  Linking
 } from 'react-native';
 
-import QRCodeScanner from 'react-native-qrcode-scanner';
-import { RNCamera } from 'react-native-camera';
+// import slider for the tuning of pitch and speed
+import Slider from '@react-native-community/slider';
+
+// import Tts Text to Speech
+import Tts from 'react-native-tts';
 
 const App = () => {
-	
-  const onSuccess = e => {
-    Linking.openURL(e.data).catch(err =>
-      console.error('An error occured', err)
+  const [voices, setVoices] = useState([]);
+  const [ttsStatus, setTtsStatus] = useState('initiliazing');
+  const [selectedVoice, setSelectedVoice] = useState(null);
+  const [speechRate, setSpeechRate] = useState(0.5);
+  const [speechPitch, setSpeechPitch] = useState(1);
+  const [
+    text,
+    setText
+  ] = useState('Enter Text like Hello About React');
+
+  useEffect(() => {
+    Tts.addEventListener(
+      'tts-start',
+      (_event) => setTtsStatus('started')
+    );
+    Tts.addEventListener(
+      'tts-finish',
+      (_event) => setTtsStatus('finished')
+    );
+    Tts.addEventListener(
+      'tts-cancel',
+      (_event) => setTtsStatus('cancelled')
+    );
+    Tts.setDefaultRate(speechRate);
+    Tts.setDefaultPitch(speechPitch);
+    Tts.getInitStatus().then(initTts);
+    return () => {
+      Tts.removeEventListener(
+        'tts-start',
+        (_event) => setTtsStatus('started')
+      );
+      Tts.removeEventListener(
+        'tts-finish',
+        (_event) => setTtsStatus('finished'),
+      );
+      Tts.removeEventListener(
+        'tts-cancel',
+        (_event) => setTtsStatus('cancelled'),
+      );
+    };
+  }, []);
+
+  const initTts = async () => {
+    const voices = await Tts.voices();
+    const availableVoices = voices
+      .filter((v) => !v.networkConnectionRequired && !v.notInstalled)
+      .map((v) => {
+        return {id: v.id, name: v.name, language: v.language};
+      });
+    let selectedVoice = null;
+    if (voices && voices.length > 0) {
+      selectedVoice = voices[0].id;
+      try {
+        await Tts.setDefaultLanguage(voices[0].language);
+      } catch (err) {
+        //Samsung S9 has always this error:
+        //"Language is not supported"
+        console.log(`setDefaultLanguage error `, err);
+      }
+      await Tts.setDefaultVoice(voices[0].id);
+      setVoices(availableVoices);
+      setSelectedVoice(selectedVoice);
+      setTtsStatus('initialized');
+    } else {
+      setTtsStatus('initialized');
+    }
+  };
+
+  const readText = async () => {
+    Tts.stop();
+    Tts.speak(text);
+  };
+
+  const updateSpeechRate = async (rate) => {
+    await Tts.setDefaultRate(rate);
+    setSpeechRate(rate);
+  };
+
+  const updateSpeechPitch = async (rate) => {
+    await Tts.setDefaultPitch(rate);
+    setSpeechPitch(rate);
+  };
+
+  const onVoicePress = async (voice) => {
+    try {
+      await Tts.setDefaultLanguage(voice.language);
+    } catch (err) {
+      // Samsung S9 has always this error: 
+      // "Language is not supported"
+      console.log(`setDefaultLanguage error `, err);
+    }
+    await Tts.setDefaultVoice(voice.id);
+    setSelectedVoice(voice.id);
+  };
+
+  const renderVoiceItem = ({item}) => {
+    return (
+      <TouchableOpacity
+        style={{
+          backgroundColor: selectedVoice === item.id ? 
+          '#DDA0DD' : '#5F9EA0',
+        }}
+        onPress={() => onVoicePress(item)}>
+        <Text style={styles.buttonTextStyle}>
+          {`${item.language} - ${item.name || item.id}`}
+        </Text>
+      </TouchableOpacity>
     );
   };
 
-  /*
-    const onSuccess = (e) => {
-    console.log(e.data);
-    // e.data contains the QR code data
-  };
-
   return (
-    <QRCodeScanner onRead={onSuccess} />
-  );
-   */
-
-  return (
-    <QRCodeScanner
-      onRead={this.onSuccess}
-      flashMode={RNCamera.Constants.FlashMode.torch}
-      topContent={
-        <Text style={styles.centerText}>
-          Go to{' '}
-          <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text> on
-          your computer and scan the QR code.
+    <SafeAreaView style={styles.container}>
+      <View style={styles.container}>
+        <Text style={styles.titleText}>
+          Text to Speech Conversion with Natural Voices
         </Text>
-      }
-      bottomContent={
-        <TouchableOpacity style={styles.buttonTouchable}>
-          <Text style={styles.buttonText}>OK. Got it!</Text>
+        <View style={styles.sliderContainer}>
+          <Text style={styles.sliderLabel}>
+            {`Speed: ${speechRate.toFixed(2)}`}
+          </Text>
+          <Slider
+            style={styles.slider}
+            minimumValue={0.01}
+            maximumValue={0.99}
+            value={speechRate}
+            onSlidingComplete={updateSpeechRate}
+          />
+        </View>
+        <View style={styles.sliderContainer}>
+          <Text style={styles.sliderLabel}>
+            {`Pitch: ${speechPitch.toFixed(2)}`}
+          </Text>
+          <Slider
+            style={styles.slider}
+            minimumValue={0.5}
+            maximumValue={2}
+            value={speechPitch}
+            onSlidingComplete={updateSpeechPitch}
+          />
+        </View>
+        <Text style={styles.sliderContainer}>
+          {`Selected Voice: ${selectedVoice || ''}`}
+        </Text>
+        <TextInput
+          style={styles.textInput}
+          onChangeText={(text) => setText(text)}
+          value={text}
+          onSubmitEditing={Keyboard.dismiss}
+        />
+        <TouchableOpacity
+          style={styles.buttonStyle}
+          onPress={readText}>
+          <Text style={styles.buttonTextStyle}>
+            Click to Read Text ({`Status: ${ttsStatus || ''}`})
+          </Text>
         </TouchableOpacity>
-      }
-    />
+        <Text style={styles.sliderLabel}>
+          Select the Voice from below
+        </Text>
+        <FlatList
+          style={{width: '100%', marginTop: 5}}
+          keyExtractor={(item) => item.id}
+          renderItem={renderVoiceItem}
+          extraData={selectedVoice}
+          data={voices}
+        />
+      </View>
+    </SafeAreaView>
   );
-
-const {t, i18n} = useTranslation();
-
-const [currentLanguage,setLanguage] =useState('en');
-
-const changeLanguage = value => {
-	i18n
-	.changeLanguage(value)
-	.then(() => setLanguage(value))
-	.catch(err => console.log(err));
 };
-
-return (
-<View
-		style={{
-		flex: 1,
-		backgroundColor: 'white',
-		alignItems: 'center',
-		justifyContent: 'space-evenly',
-		}}>
-		<Text style={{fontWeight: 'bold', fontSize: 25, color: '#33A850'}}>
-		{t('hello')}{' '}
-		</Text>
-		<Text style={{fontWeight: 'bold', fontSize: 25, color: '#33A850'}}>
-		{t('this line is translated')}
-		</Text>
-		<Pressable
-		onPress={() => changeLanguage('en')}
-		style={{
-			backgroundColor:
-			currentLanguage === 'en' ? '#33A850' : '#d3d3d3',
-			padding: 20,
-		}}>
-		<Text>Select English</Text>
-		</Pressable>
-		<Pressable
-		onPress={() => changeLanguage('hi')}
-		style={{
-			backgroundColor:
-			currentLanguage === 'hi' ? '#33A850' : '#d3d3d3',
-			padding: 20,
-		}}>
-		<Text>हिंदी का चयन करें</Text>
-		</Pressable>
-	</View>
-);
-};
-const styles = StyleSheet.create({
-  centerText: {
-    flex: 1,
-    fontSize: 18,
-    padding: 32,
-    color: '#777'
-  },
-  textBold: {
-    fontWeight: '500',
-    color: '#000'
-  },
-  buttonText: {
-    fontSize: 21,
-    color: 'rgb(0,122,255)'
-  },
-  buttonTouchable: {
-    padding: 16
-  }
-});
 
 export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    padding: 5,
+  },
+  titleText: {
+    fontSize: 22,
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+  buttonStyle: {
+    justifyContent: 'center',
+    marginTop: 15,
+    padding: 10,
+    backgroundColor: '#8ad24e',
+  },
+  buttonTextStyle: {
+    color: '#fff',
+    textAlign: 'center',
+  },
+  sliderContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 300,
+    padding: 5,
+  },
+  sliderLabel: {
+    textAlign: 'center',
+    marginRight: 20,
+  },
+  slider: {
+    flex: 1,
+  },
+  textInput: {
+    borderColor: 'gray',
+    borderWidth: 1,
+    color: 'black',
+    width: '100%',
+    textAlign: 'center',
+    height: 40,
+  },
+});
