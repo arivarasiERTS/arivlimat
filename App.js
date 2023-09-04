d758316135411eb0bc70fe0c8ded42bec88e66d5
arivarasiERTS <arivarasiERTS@gmail.com>
2023-09-04 12:37:40 +0530

    FETCH and Axios

--- App.js
+++ App.js
@@ -1,42 +1,14 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- */
+import React from 'react';
 
-import React, { useCallback, useEffect } from 'react';
-import { StyleSheet, useColorScheme, View } from 'react-native';
-import { HomeScreen } from './app/modules';
-import { get, save } from './app/modules/Storage';
+import ApiContainer from './App/Screens/ApiContainer';
 
 const App = () => {
-  const appearance = useColorScheme();
-  const setAppTheme = useCallback(async () => {
-    const IS_FIRST = await get('IS_FIRST');
-    console.log(IS_FIRST);
-    if (IS_FIRST === null) {
-      save('Theme', appearance);
-      save('IsDefault', true);
-      save('IS_FIRST', true);
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  useEffect(() => {
-    setAppTheme();
-  }, [setAppTheme]);
-
   return (
-    <View style={styles.container}>
-      <HomeScreen />
-    </View>
+    <>
+      <ApiContainer />
+    </>
   );
 };
 
-export default App;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
+export default App;
