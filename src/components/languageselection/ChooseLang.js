908c10676677b1f692c272113f5f7dce31bbf9ea
arivarasiERTS <arivarasiERTS@gmail.com>
2023-10-06 09:53:31 +0530

    rndevelopment

---
+++ src/components/languageselection/ChooseLang.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {SafeAreaView,ScrollView,StyleSheet,Text,View} from 'react-native';
+
+function ChooseLang(){
+
+return (
+    <SafeAreaView>
+      <ScrollView>
+      <View>
+      <Text>Welcome</Text> 
+      </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  Container: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+});
+
+export default ChooseLang;
